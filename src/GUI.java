e4aff79c04dffd613947b51734bcc31b26638828
Julien Law <julienlaw1@gmail.com>
2013-10-12 15:28:06 -0700

    added GUI and VPN_Controller classes/java files and created a simple GUI for toggling between server/client with no functionality

---
+++ src/GUI.java
@@ -0,0 +1,62 @@
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.*;
+
+public class GUI extends JFrame {
+	
+	/** Variables **/
+	private JLabel frame_title;
+	private JRadioButton client_toggle;
+	private JRadioButton server_toggle;
+	private JButton toggle_confirm;
+	private JPanel buttons_area;
+	private Toolkit toolkit;
+	private Dimension screen;
+	/** End of Variables **/
+	
+	public GUI() {
+		
+		// set the window
+		setSize(300, 200);
+		toolkit = getToolkit();
+		screen = toolkit.getScreenSize();
+		setLocation(screen.width / 2 - getWidth() / 2, screen.height / 2 - getHeight() / 2);
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setVisible(true);
+		setLayout(new BorderLayout());
+		define_variables();
+		
+	}
+	
+	/**
+	 * define_variables function
+	 * This function defines the variables for the GUI
+	 */
+	private void define_variables() {
+		JPanel buttons_area = new JPanel();
+		buttons_area.setLayout(new GridLayout(3, 1));
+		
+		// define the buttons and title
+		frame_title = new JLabel("VPN Client/Server Toggle");
+		frame_title.setHorizontalAlignment(frame_title.CENTER);
+		client_toggle = new JRadioButton("Client");
+		server_toggle = new JRadioButton("Server");
+		toggle_confirm = new JButton("Submit");
+		
+		// add the radios to a button group
+		ButtonGroup group = new ButtonGroup();
+		group.add(client_toggle);
+		group.add(server_toggle);
+		
+		// add the buttons and the title to the window
+		add(frame_title, BorderLayout.NORTH);
+		buttons_area.add(client_toggle);
+		buttons_area.add(server_toggle);
+		buttons_area.add(toggle_confirm);
+		add(buttons_area);
+		
+	}
+	
+	
+	
+}
---
+++ src/VPN_Controller.java
@@ -0,0 +1,13 @@
+
+public class VPN_Controller {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		
+		GUI the_gui = new GUI();
+		
+	}
+
+}
