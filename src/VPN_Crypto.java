b7e1eaa57979c581b68813d7305bad4a275ec0c6
Julien Law <julienlaw1@gmail.com>
2013-10-13 02:10:30 -0700

    added crypto class to do symmetric crypto to some plaintext

--- src/VPN_Crypto.java
+++ src/VPN_Crypto.java
@@ -1,12 +1,82 @@
+import javax.crypto.*;
+import javax.crypto.spec.IvParameterSpec;
+import javax.crypto.spec.PBEKeySpec;
+import javax.crypto.spec.SecretKeySpec;
+
+import java.security.*;
+import java.util.*;
+
+/**
+ * 
+ * @author Julien
+ * This class is responsible for doing all of the encryption and decryption 
+ * 
+ * Algorithm:
+ * 1. get the plain text
+ * 2. generate the key
+ * 3. pass into the block cipher
+ * 4. get the cipher text 
+ */
 
 public class VPN_Crypto {
 
+	private byte[] iv;
+	
+	public VPN_Crypto() {
+		try {
+			SecretKey my_key = generate_secret_key("yolo");
+			System.out.println("key = yolo");
+			System.out.println("plaintext = my cleartext");
+			byte[] cipher = encrypt_AES(my_key);
+			System.out.println("-------");
+			
+			String plaintext = decrypt_AES(my_key, cipher);
+			System.out.println(plaintext);
+			
+		} 
+		catch(Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+			System.out.println(e.getMessage());
+		}
+		
+	}
+	
+	// http://stackoverflow.com/questions/4319496/how-to-encrypt-and-decrypt-data-in-java
+	// http://www.coderanch.com/t/525677/Security/AES-decryption-InvalidKeyException-Parameters-missing
+	public byte[] encrypt_AES(SecretKey key) throws Exception {
+		Cipher aes = Cipher.getInstance("AES/CBC/PKCS5Padding");
+		aes.init(Cipher.ENCRYPT_MODE, key);
+		iv = aes.getIV();
+		
+		return aes.doFinal("my cleartext".getBytes());
+	}
+	
+	public String decrypt_AES(SecretKey key, byte[] ciphertext) throws Exception {
+		Cipher aes = Cipher.getInstance("AES/CBC/PKCS5Padding");
+		aes.init(Cipher.DECRYPT_MODE, key, new IvParameterSpec(iv));
+		
+		return new String(aes.doFinal(ciphertext));
+	}
+	
 	/**
-	 * @param args
+	 * generate_secret_key function
+	 * This function uses the SecretKeyFactory to generate a key with a salt
+	 * @throws Exception 
 	 */
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+	public SecretKey generate_secret_key(String input_key) throws Exception {
+		
+		// hash a passphrase with SHA1
+		String passphrase = "Julien is the best";
+		MessageDigest digest = MessageDigest.getInstance("MD5");
+		digest.update(passphrase.getBytes());
+		
+		// generate key with a salt
+		byte[] salt = digest.digest();
+		int iterations = 10000;
+		SecretKeyFactory factory = SecretKeyFactory.getInstance("PBKDF2WithHmacSHA1");
+		SecretKey temp = factory.generateSecret(new PBEKeySpec(input_key.toCharArray(), salt, iterations, 128));
+		return new SecretKeySpec(temp.getEncoded(), "AES");
 	}
 
 }
