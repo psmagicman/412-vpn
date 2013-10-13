06d561b76690fc9ea116203a3c4d9c83d12ddebc
Julien Law <julienlaw1@gmail.com>
2013-10-12 22:09:46 -0700

    added actionlisteners to the radio buttons

--- src/GUI.java
+++ src/GUI.java
@@ -20,11 +20,15 @@ public class GUI extends JFrame {
 		setSize(300, 200);
 		toolkit = getToolkit();
 		screen = toolkit.getScreenSize();
+		// set the location of the window to the middle of the screen
 		setLocation(screen.width / 2 - getWidth() / 2, screen.height / 2 - getHeight() / 2);
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		setLayout(new BorderLayout());
 		define_variables();
+		setTitle(frame_title.getText());
 		setVisible(true);
+		
+		gui_listener();
 	}
 	
 	/**
@@ -37,7 +41,7 @@ public class GUI extends JFrame {
 		
 		// define the buttons and title
 		frame_title = new JLabel("VPN Client/Server Toggle");
-		frame_title.setHorizontalAlignment(frame_title.CENTER);
+		//frame_title.setHorizontalAlignment(frame_title.CENTER);
 		client_toggle = new JRadioButton("Client");
 		server_toggle = new JRadioButton("Server");
 		toggle_confirm = new JButton("Submit");
@@ -48,10 +52,30 @@ public class GUI extends JFrame {
 		group.add(server_toggle);
 		
 		// add the buttons and the title to the window
-		add(frame_title, BorderLayout.NORTH);
+		//add(frame_title, BorderLayout.NORTH);
 		buttons_area.add(client_toggle);
 		buttons_area.add(server_toggle);
 		buttons_area.add(toggle_confirm);
 		add(buttons_area);
+	}
+	
+	/**
+	 * gui_listener function
+	 * This function is responsible for actionlistener of the radio buttons
+	 */
+	private void gui_listener() {
+		
+		client_toggle.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				System.out.println("client");
+			}
+		});
+		
+		server_toggle.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				System.out.println("server");
+			}
+		});
+		
 	}
 }
