0be21284ccef643876f90ab27b190765bd982b97
Julien Law <julienlaw1@gmail.com>
2013-10-12 23:09:21 -0700

    added proper functionality to server actionlistener to go to the server gui

--- src/GUI.java
+++ src/GUI.java
@@ -16,11 +16,11 @@ public class GUI extends JFrame {
 	
 	public GUI() {
 		
-		// set the window
+		// set the frame size
 		setSize(300, 200);
 		toolkit = getToolkit();
 		screen = toolkit.getScreenSize();
-		// set the location of the window to the middle of the screen
+		// set the location of the frame to the middle of the screen
 		setLocation(screen.width / 2 - getWidth() / 2, screen.height / 2 - getHeight() / 2);
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		setLayout(new BorderLayout());
@@ -41,7 +41,6 @@ public class GUI extends JFrame {
 		
 		// define the buttons and title
 		frame_title = new JLabel("VPN Client/Server Toggle");
-		//frame_title.setHorizontalAlignment(frame_title.CENTER);
 		client_toggle = new JRadioButton("Client");
 		server_toggle = new JRadioButton("Server");
 		toggle_confirm = new JButton("Submit");
@@ -51,8 +50,7 @@ public class GUI extends JFrame {
 		group.add(client_toggle);
 		group.add(server_toggle);
 		
-		// add the buttons and the title to the window
-		//add(frame_title, BorderLayout.NORTH);
+		// add the buttons to the frame
 		buttons_area.add(client_toggle);
 		buttons_area.add(server_toggle);
 		buttons_area.add(toggle_confirm);
@@ -73,7 +71,8 @@ public class GUI extends JFrame {
 		
 		server_toggle.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
-				System.out.println("server");
+				setVisible(false);
+				GUI_Server server_gui = new GUI_Server();
 			}
 		});
 		
