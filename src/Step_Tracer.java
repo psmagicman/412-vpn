e6a2b74abb3bb006f9ab04306ebf8f3f6bc1af43
Julien Law <julienlaw1@gmail.com>
2013-10-15 14:17:38 -0700

    changed ArrayList to Vector and removed some printlns

--- src/Step_Tracer.java
+++ src/Step_Tracer.java
@@ -11,7 +11,7 @@ public class Step_Tracer {
 
 	/** Variables **/
 	
-	private ArrayList<String> wire_data;
+	private Vector<String> wire_data;
 	private int continue_counter;
 	private int num_steps;
 	
@@ -22,21 +22,34 @@ public class Step_Tracer {
 		System.out.println("num_steps = " + num_steps + " continue_counter = " + continue_counter);
 	}
 	
+	/**
+	 * 
+	 */
 	private void reset_step_tracer() {
-		wire_data = new ArrayList<String>();
+		wire_data = new Vector<String>();
 		continue_counter = 0;
 		num_steps = 0;
 	}
 	
+	/**
+	 * 
+	 * @param step
+	 */
 	public void insert_steps(String step) {
 		//wire_data.append(step);
-		System.out.println(step);
+		//System.out.println(step);
 		wire_data.add(num_steps, step);
 		//num_steps++;
 	}
 	
+	/**
+	 * 
+	 * @return
+	 * @throws NullPointerException
+	 */
 	public String print_steps() throws NullPointerException {
-		System.out.println(wire_data.get(continue_counter));
+		// TODO: check if the vector is empty
+		//System.out.println(wire_data.get(continue_counter));
 		String data = wire_data.get(continue_counter);
 		//continue_counter++;
 		return data;
