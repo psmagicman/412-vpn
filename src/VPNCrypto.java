a01cf0817a1fa0d4e72157208716b4fe8a6e39c5
dennistjandra <dennistjandra@gmail.com>
2013-10-15 13:45:06 -0700

    RSA Integration and Use As Utility Class

--- src/VPNCrypto.java
+++ src/VPNCrypto.java
@@ -3,6 +3,7 @@ import javax.crypto.spec.IvParameterSpec;
 import javax.crypto.spec.PBEKeySpec;
 import javax.crypto.spec.SecretKeySpec;
 
+import java.io.UnsupportedEncodingException;
 import java.security.*;
 import java.util.*;
 
@@ -25,10 +26,10 @@ import java.util.*;
 			System.out.println("message = " + message);
 			byte[] cipher = encrypt_AES(my_key, message);
 			System.out.println("-------");
-			
+
 			String plaintext = decrypt_AES(my_key, cipher);
 			System.out.println(plaintext);
-			
+
 		} 
 		catch(Exception e) {
 			// TODO Auto-generated catch block
@@ -39,12 +40,12 @@ import java.util.*;
 
 public class VPNCrypto {
 
-	private byte[] iv;
+	private static byte[] iv;
 
 	public VPNCrypto() {
 
 	}
-	
+
 	// http://stackoverflow.com/questions/4319496/how-to-encrypt-and-decrypt-data-in-java
 	// http://www.coderanch.com/t/525677/Security/AES-decryption-InvalidKeyException-Parameters-missing
 	/**
@@ -53,15 +54,15 @@ public class VPNCrypto {
 	 * @return
 	 * @throws Exception
 	 */
-	public byte[] encrypt_AES(SecretKey key, String message) throws Exception {
+	public static byte[] encrypt_AES(SecretKey key, String message) throws Exception {
 		Cipher aes = Cipher.getInstance("AES/CBC/PKCS5Padding");
 		aes.init(Cipher.ENCRYPT_MODE, key);
 		// need to have the iv carry over or else there will be an invalid key exception thrown at the decryption phase
 		iv = aes.getIV();
-		
+
 		return aes.doFinal(message.getBytes());
 	}
-	
+
 	/**
 	 * 
 	 * @param key
@@ -69,13 +70,13 @@ public class VPNCrypto {
 	 * @return
 	 * @throws Exception
 	 */
-	public String decrypt_AES(SecretKey key, byte[] ciphertext) throws Exception {
+	public static String decrypt_AES(SecretKey key, byte[] ciphertext) throws Exception {
 		Cipher aes = Cipher.getInstance("AES/CBC/PKCS5Padding");
 		aes.init(Cipher.DECRYPT_MODE, key, new IvParameterSpec(iv));
-		
+
 		return new String(aes.doFinal(ciphertext));
 	}
-	
+
 	/**
 	 * generate_secret_key function
 	 * This function uses the SecretKeyFactory to generate a key with a salt
@@ -83,19 +84,103 @@ public class VPNCrypto {
 	 * @return
 	 * @throws Exception 
 	 */
-	public SecretKey generate_secret_key(String input_key) throws Exception {
-		
+	public static SecretKey generate_secret_key(String input_key) throws Exception {
+
 		// hash the passphrase with some message digest algorithm
 		String passphrase = "Julien is the best";		// change the passphrase?
 		MessageDigest digest = MessageDigest.getInstance("SHA-512");
 		digest.update(passphrase.getBytes());
-		
+
 		// generate key with a salt
 		byte[] salt = digest.digest();
 		int iterations = 10000;
 		SecretKeyFactory factory = SecretKeyFactory.getInstance("PBKDF2WithHmacSHA1");
 		SecretKey temp = factory.generateSecret(new PBEKeySpec(input_key.toCharArray(), salt, iterations, 128));
 		return new SecretKeySpec(temp.getEncoded(), "AES");
+	}
+
+	/**
+	 * Generates an RSA keypair
+	 * @return the rsa keypair
+	 */
+	public static KeyPair generateRSAKeys () {
+		KeyPairGenerator keyGen;
+		try {
+			keyGen = KeyPairGenerator.getInstance("RSA");
+			keyGen.initialize(512);
+			KeyPair keys = keyGen.genKeyPair();
+			return keys;
+		} catch (NoSuchAlgorithmException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		return null;
+	}
+	/** 
+	 * Encrypt message with RSA
+	 * @param publicKey the public key used for encrypting
+	 * @param plaintext the plaintext to encrypt
+	 * @return byte array containing ciphertext of plaintext
+	 */
+	public static byte[] encrypt_RSA(Key publicKey, String plaintext){
+		Cipher rsa;
+		try {
+			rsa = Cipher.getInstance("RSA");
+			rsa.init(Cipher.ENCRYPT_MODE, publicKey);
+			byte[] encrypted  = rsa.doFinal(plaintext.getBytes());
+			return encrypted;
+
+		} catch (NoSuchAlgorithmException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (NoSuchPaddingException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (InvalidKeyException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (IllegalBlockSizeException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (BadPaddingException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		return null;
+	}
+
+	/**
+	 * Decrypt RSA encrypted ciphertext
+	 * @param privateKey the private key to decrypt with
+	 * @param ciphertext the ciphertext that will be decrypted
+	 * @return the plaintext
+	 */
+	public static String decrypt_RSA(Key privateKey, byte[] ciphertext){
+		Cipher rsa;
+		try {
+			rsa = Cipher.getInstance("RSA");
+	 		rsa.init(Cipher.DECRYPT_MODE, privateKey);
+	 		return new String (rsa.doFinal(ciphertext), "UTF-8");
+		} catch (NoSuchAlgorithmException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (NoSuchPaddingException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (InvalidKeyException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (UnsupportedEncodingException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (IllegalBlockSizeException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (BadPaddingException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		return null;
 	}
 
 }
