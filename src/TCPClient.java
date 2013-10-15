d8bde5fe114d4ae3c3499e377ceea40d5db6cd44
dennistjandra <dennistjandra@gmail.com>
2013-10-15 15:04:02 -0700

    Added Auth methods to TCP* classes.
    WARNING: these are not tested yet and expected to have errors - just the
    core logic is there with some comments. Let me know if anything looks
    wrong = D
    Auth steps are laid out in comments

--- src/TCPClient.java
+++ src/TCPClient.java
@@ -1,7 +1,13 @@
 import java.net.*;
+import java.security.KeyPair;
 import java.io.*;
 
+import javax.crypto.SecretKey;
+
 public class TCPClient {
+	String sharedSecret = "REPLACE ME FROM GUI =D";
+	KeyPair keys;
+	SecretKey aesKey;
 	public void main (String args[])
 	{
 		// values specified from the GUI
@@ -20,5 +26,57 @@ public class TCPClient {
 		} catch (IOException e){
 			System.out.println("IOException Client: " + e.getMessage());
 		}
+	}
+	
+	/**
+	 *  This function will be called in tandem with the corresponding auth in TCPServer.java
+	 * @param skt
+	 * @return
+	 */
+	public boolean auth(Socket skt) {
+        try {
+			ObjectInputStream ois = new ObjectInputStream(skt.getInputStream());
+			ObjectOutputStream oos = new ObjectOutputStream(skt.getOutputStream());
+
+        	//first generate the RSA keypair
+			keys = VPNCrypto.generateRSAKeys();
+			//next ,need to send over public key so that first phase auth (server send back secret) can occur
+			oos.writeObject(keys.getPublic());
+			//receive the encrypted secret pass phrase and symm key and check its validity, if it checks out, 
+			//then respond with the secret encoded with symm key
+			int encryptedSize = ois.readInt();
+			byte[] encrypted = new byte[encryptedSize];
+			ois.read(encrypted);
+			aesKey = (SecretKey) ois.readObject();
+			if(VPNCrypto.decrypt_RSA(keys.getPrivate(), encrypted).equals(sharedSecret)){
+				byte[] respEncoded = VPNCrypto.encrypt_AES(aesKey, sharedSecret);
+				oos.writeInt(respEncoded.length);
+				oos.write(respEncoded);
+			}
+			else {
+				byte[] failMessage = "FAILED".getBytes();
+				oos.writeInt(failMessage.length);
+				oos.write(failMessage);
+				return false;
+			}
+			//second phase - expect back response from server seeing if secret phrase encoded with symm key was right
+			String secondPhaseStatus = ois.readUTF();
+			if (secondPhaseStatus.equals("PASS"))
+				return true;
+			else
+				return false;
+				
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (ClassNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+
+		return false;
 	}
 }
--- src/TCPServer.java
+++ src/TCPServer.java
@@ -1,7 +1,13 @@
 import java.net.*;
+import java.security.Key;
 import java.io.*;
 
+import javax.crypto.SecretKey;
+
 public class TCPServer {
+	String sharedSecret = "REPLACE ME FROM GUI =D";
+	Key publicKey;
+	SecretKey aesKey;
 	public void main (String args[])
 	{
 		// port number needs to be grabbed from the GUI
@@ -11,11 +17,8 @@ public class TCPServer {
 			ServerSocket listenerSocket = new ServerSocket(port);
 			
 			System.out.println("Started listening");
-			while(true) {
-				Socket clientSocket = listenerSocket.accept();
-				// print string
-				printClientString(clientSocket);
-			}
+			Socket clientSocket = listenerSocket.accept();
+			printClientString(clientSocket);
 		} catch(IOException e) {
 			System.out.println("IOexception: " + e.getMessage());
 		}
@@ -34,5 +37,56 @@ public class TCPServer {
 		} catch(IOException e) {
 			System.out.println("IOexception Server: " + e.getMessage());
 		}
+	}
+	
+	/**
+	 * This function will be called in tandem with the corresponding auth in TCPCLient.java
+	 * @param skt
+	 * @return
+	 */
+	public boolean auth(Socket skt) {
+		try {
+			aesKey = VPNCrypto.generate_secret_key("WHAT IS SUPPOSED TO GO HERE?");
+			ObjectInputStream ois = new ObjectInputStream(skt.getInputStream());
+			ObjectOutputStream oos = new ObjectOutputStream(skt.getOutputStream());
+
+			//receive the public key from the client who generates the keypair first
+			publicKey = (Key) ois.readObject();
+			//then, send back the shared secret to confirm identity and send over symmetric key
+			byte[] encrypted = VPNCrypto.encrypt_RSA(publicKey, sharedSecret);
+			oos.writeInt(encrypted.length);
+			oos.write(encrypted);
+			oos.writeObject(aesKey);
+			//expect back a new byte array which says if auth failed, or if it passed then the secret phase encoded with the symm key
+			//send back second phase pass/fail
+			int respSize = ois.readInt();
+			byte[] resp = new byte[respSize];
+			ois.read(resp);
+			if (new String(resp, "UTF8").equals("FAILED")){
+				return false;
+			}
+			else {
+				if( VPNCrypto.decrypt_AES(aesKey, resp).equals(sharedSecret)){
+					oos.writeUTF("PASS");
+					return true;
+				}
+				else {
+					oos.writeUTF("FAILED");
+					return false;
+				}
+				
+			}
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (ClassNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+
+		return false;
 	}
 }
