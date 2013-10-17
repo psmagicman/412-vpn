4e779221922af36047b2889761e09df2f14578db
dennistjandra <dennistjandra@gmail.com>
2013-10-16 18:57:38 -0700

    Signature functions added in

--- src/VPNAuth.java
+++ src/VPNAuth.java
@@ -1,3 +1,10 @@
+import java.security.InvalidKeyException;
+import java.security.NoSuchAlgorithmException;
+import java.security.PrivateKey;
+import java.security.PublicKey;
+import java.security.Signature;
+import java.security.SignatureException;
+
 
 public class VPNAuth {
 
@@ -5,8 +12,38 @@ public class VPNAuth {
 	 * @param args
 	 */
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
 	}
-
+	
+	public static byte[] computeSignature(PrivateKey privateKey, byte[] message){
+		try {
+			Signature sig = Signature.getInstance("SHA1withRSA");
+			sig.initSign(privateKey);
+			sig.update(message);
+			byte[] signature = sig.sign();
+			return signature;
+		} catch (NoSuchAlgorithmException e) {
+			e.printStackTrace();
+		} catch (InvalidKeyException e) {
+			e.printStackTrace();
+		} catch (SignatureException e) {
+			e.printStackTrace();
+		}
+		return null;
+	}
+	
+	public static boolean verifySignature(PublicKey pubKey, byte[] message, byte[] signature){
+		try {
+			Signature sig = Signature.getInstance("SHA1withRSA");
+			sig.initVerify(pubKey);
+			sig.update(message);
+			return sig.verify(signature);
+		} catch (NoSuchAlgorithmException e) {
+			e.printStackTrace();
+		} catch (InvalidKeyException e) {
+			e.printStackTrace();
+		} catch (SignatureException e) {
+			e.printStackTrace();
+		}
+		return false;
+	}
 }
